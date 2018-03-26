07f3a216cfefec033dff4005eab38b82d6172755
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-26 00:03:02 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  attr_reader :name
+
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @all << self
+  end
+
+  def self.all
+    @@all.each do |x|
+      puts x.name
+    end
+  end
+
+end
